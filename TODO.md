3a7b7193607c9998dc112d9c3c03cd6f52502e31
wakewakame <0214.taiki@gmail.com>
2021-06-19 06:04:36 +0900

    一旦コミット

---
+++ TODO.md
@@ -0,0 +1,5 @@
+# Todo
+- imu,gps,cameraをそれぞれ読み込むためのプログラムを書く
+- examplesを追加
+- cinderで三角形を描画
+- QuadSLAMとCinderを統合
---
+++ include/quadslam.h
@@ -0,0 +1,46 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <stdint.h>
+#include "opencv2/opencv.hpp"
+#include "opencv2/videoio.hpp"
+#include "zlib.h"
+
+namespace qs {
+	struct FrameInfo {
+		uint64_t frameNumber;
+		double timestamp;
+		uint8_t isColorFrameExist, isDepthFrameExist, isConfidenceFrameExist;
+		uint64_t depthOffset;
+		uint64_t confidenceOffset;
+		float intrinsics[3][3];
+		float projectionMatrix[4][4];
+		float viewMatrix[4][4];
+	};
+
+	class QuadSLAM {
+	public:
+		QuadSLAM(const std::string& recDirPath) {
+			const std::string
+				infoJsonPath        = recDirPath + "/info.json",
+				cameraFrameInfoPath = recDirPath + "/cameraFrameInfo",
+				cameraPath          = recDirPath + "/camera.mp4",
+				depthPath           = recDirPath + "/depth",
+				confidencePath      = recDirPath + "/confidence",
+				imuPath             = recDirPath + "/imu",
+				gpsPath             = recDirPath + "/gps";
+
+			std::ifstream infoJsonFs(infoJsonPath, std::ios_base::in | std::ios_base::binary);
+			if (infoJsonFs.is_open()) {
+				std::string infoJson((std::istreambuf_iterator<char>(infoJsonFs)), std::istreambuf_iterator<char>());
+				std::cout << infoJson << std::endl;
+				infoJsonFs.close();
+			}
+		}
+		virtual ~QuadSLAM() {}
+
+		void next() {
+		
+		}
+	};
+}
